a56097836a19d6002474985bfed8d3b61fc1a032
Miguel Figuera <miguelqui725@gmail.com>
2023-08-28 22:40:31 -0400

    vitest and js-dom working

--- App.test.jsx
+++ App.test.jsx
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+// App.test.jsx
 
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "./src/App";
 
 describe("App component", () => {
   it("renders correct heading", () => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// App.jsx
-
 const App = () => <h1>Our First Test</h1>;
 
 export default App;
